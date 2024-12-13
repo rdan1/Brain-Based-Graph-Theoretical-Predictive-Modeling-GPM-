f53c94ed5d4bce843d7f912098ec62d413db3b00
rdan1 <92593725+rdan1@users.noreply.github.com>
2024-12-13 10:55:40 -0500

    Update GPM_predict.m

--- permutation_testing/GPM_predict.m
+++ permutation_testing/GPM_predict.m
@@ -4,7 +4,7 @@ function [r_work, r_null] = GPM_predict(all_phenotype, all_graph, all_covariate1
 % null model: includes only covariates (i.e., all predictors except for graph-theoretical measures)
 
 y = all_phenotype;                                                              % outcome. dimensions: subj X 1
-X_null = [all_covariate1 all_covariate2];                              % covariates. dimensions: subj X variables
+X_null = [all_covariate1 all_covariate2];                                       % covariates. dimensions: subj X variables
         
 %% ---------------LEAVE ONE OUT CROSS VALIDATION (LOOCV)-------------------- 
 % In each iteration, the data is divided into a training set and a testing set. Model is built on training set and prediction is done for testing set.
@@ -17,26 +17,26 @@ for i = 1:n
         
             XTrain_null = X_null;
             XTrain_null(i,:) = [];                 % Train set: N-1 (excluding one participant)
-            XTest_null = X_null(i,:);           % Test set: the participant who was excluded from model training
+            XTest_null = X_null(i,:);              % Test set: the participant who was excluded from model training
         
             train_graph = all_graph;
-            train_graph(:,i) = [];                % Train set: N-1 (excluding one participant)
-            test_graph = all_graph(:,i);     % Test set: the participant who was excluded from model training
+            train_graph(:,i) = [];                 % Train set: N-1 (excluding one participant)
+            test_graph = all_graph(:,i);           % Test set: the participant who was excluded from model training
         
             yTrain = y;
-            yTrain(i,:) = [];                          % Train set: N-1 (excluding one participant)
-            yTest = y(i,:);                            % Test set: the participant who was excluded from model training
+            yTrain(i,:) = [];                      % Train set: N-1 (excluding one participant)
+            yTest = y(i,:);                        % Test set: the participant who was excluded from model training
         
            %% ---------------(1) WORKING MODEL---------------------   
            %---------------BUILDING MODEL ON TRAINING DATA-------
            % step 1: (internal nested loop) multiple regression model for each graph measure. Model can include interactions between graph metric and covariate (e.g., sex, group).
            for j = 1:num_graph
                 XTrain = [train_graph(j,:)' XTrain_null];
-                mdl{j} = fitlm(XTrain,yTrain);                 % can include here interactions; defining categorical variables (e.g., group, sex)
+                mdl{j} = fitlm(XTrain,yTrain);                  % can include here interactions; defining categorical variables (e.g., group, sex)
                 anv{j} = anova(mdl{j,1}, 'summary');
                 p(j,1) = anv{j,1}.pValue(2);                    % model's p value
 
-                %MSE(j,1) = mdl{j,1}.MSE;                   % alternative criterion to lowest p value: selecting model with lowest MSE. Equivalent.
+                %MSE(j,1) = mdl{j,1}.MSE;                       % alternative criterion to lowest p value: selecting model with lowest MSE. Equivalent.
            end
         
            % step 2: selecting model with lowest p value
@@ -44,12 +44,12 @@ for i = 1:n
             best_graph_vec_p(i) = best_graph_index;
         
            % step 3: saving model coefficients (betas)
-           beta_mat(i,:) = mdl{best_graph_index,1}.Coefficients.Estimate;    % model coefficients.
+           beta_mat(i,:) = mdl{best_graph_index,1}.Coefficients.Estimate;       % model coefficients.
            p_coef(i,:) = mdl{best_graph_index,1}.Coefficients.pValue;           % use this to examine which predictors were significant
         
            %---------------------TEST DATA: PREDICTION ----------------  
            XTest = [test_graph(best_graph_index,:)' XTest_null];
-           XTest0 = [ones(size(XTest,1),1) XTest];                                          % including a first col with ones for the intercept (which is included within beta_mat)
+           XTest0 = [ones(size(XTest,1),1) XTest];                              % including a first col with ones for the intercept (which is included within beta_mat)
            yhat(i,1) = XTest0*beta_mat(i,:)'; 
         
            %% ---------------------(2) NULL MODEL---------------------     
@@ -68,7 +68,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% ---------------------(1) WORKING MODEL--------------------- 
-[r_work,p_work] = corr(y, yhat,'rows','complete');                                        %only r is needed, p value is not used (this is the one without permutations)
+[r_work,p_work] = corr(y, yhat,'rows','complete');                                        % only r is needed, p value is not used (this is the one without permutations)
 
 %% ---------------------(2) NULL MODEL--------------------- 
 [r_null,p_null] = corr(y, yhat_null,'rows','complete');                                     %only r is needed, p value is not used (this is the one without permutations)
